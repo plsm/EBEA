c478052fa5a176e8ca91a3af832566daca3a6dac
Pedro Mariano <plmariano@fc.ul.pt>
2014-08-08 15:24:33 +0100

    Changes to module streams and its submodules
    Use of new functionalities in module parseable.iou
    Use of type key instead of type int to refer to players.

--- mercury/ebea.streams.playerProfile.m
+++ mercury/ebea.streams.playerProfile.m
@@ -10,20 +10,28 @@
 
 :- interface.
 
+:- import_module ebea.population, ebea.population.players.
 :- import_module game.
 :- import_module parseable.iou.
 
 :- type iterationPlayerProfileRecords --->
 	ippr(
 		iteration :: int,
-		list(list(int))
+		profiles  :: list(list(key))
 	).
 
 :- pred read(io.binary_input_stream, G, parseable.iou.cache, parseable.iou.cache, parseable.iou.result(io.result(iterationPlayerProfileRecords)), io.state, io.state)
 	<= asymmetricGame(G, C).
 :- mode read(in, in, in, out, out, di, uo) is det.
 
-:- pred write(io.binary_output_stream, G, int, list(list(int)), io.state, io.state)
+/**
+ * Reads the entire stream into a list.
+ */
+:- pred read(ebea.streams.inStreams, G, parseable.iou.ioResult(list(iterationPlayerProfileRecords)), io.state, io.state)
+	<= asymmetricGame(G, C).
+:- mode read(in(detailedBin), in, out, di, uo) is det.
+
+:- pred write(io.binary_output_stream, G, int, list(list(key)), io.state, io.state)
 	<= asymmetricGame(G, C).
 :- mode write(in, in, in, in, di, uo) is det.
 
@@ -51,6 +59,13 @@ read(Stream, Game, !Cache, RIResult, !IO) :-
 	),
 	parseable.iou.read(Stream, 4096, no, PredParser, !Cache, RIResult, !IO).
 
+read(BinStream, Game, Result, !IO) :-
+	PredParser =
+	(pred(I::out, LB1::in, LB2::out) is semidet :-
+		ebea.streams.playerProfile.parse(numberPlayers(Game), I, LB1, LB2)
+	),
+	parseable.iou.readAll(BinStream^bisPlayerProfile, 4096, no, PredParser, Result, !IO).
+
 write(Stream, Game, Iteration, PlayerProfiles, !IO) :-
 	Data = ippr(Iteration, PlayerProfiles),
 	parse(numberPlayers(Game), Data, Bytes, []),
@@ -65,7 +80,7 @@ parse(NumberPlayers, ippr(Iteration, PlayerProfiles)) -->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Implementation of private predicates and functions
 
-:- pred parsePlayerProfiles(int, list(list(int)), parseable.state, parseable.state).
+:- pred parsePlayerProfiles(int, list(list(key)), parseable.state, parseable.state).
 :- mode parsePlayerProfiles(in, in, out, in) is det.
 :- mode parsePlayerProfiles(in, out, in, out) is semidet.
 
@@ -80,14 +95,14 @@ parsePlayerProfiles(NumberPlayers::in, PlayerProfiles::out, L1::in, L2::out) :-
 	.
 
 
-:- pred parsePlayerProfileD(int, list(int), parseable.state, parseable.state).
+:- pred parsePlayerProfileD(int, list(key), parseable.state, parseable.state).
 :- mode parsePlayerProfileD(in, in, out, in) is det.
 
 parsePlayerProfileD(NumberPlayers, PlayerProfile) -->
 	parseable.parseList(onlyElements(NumberPlayers), PlayerProfile)
 	.
 
-:- pred parsePlayerProfileS(int, list(int), parseable.state, parseable.state).
+:- pred parsePlayerProfileS(int, list(key), parseable.state, parseable.state).
 :- mode parsePlayerProfileS(in, out, in, out) is semidet.
 
 parsePlayerProfileS(NumberPlayers, PlayerProfile) -->
