de4e89dca86ec8aaeb01b843f4495cc050233fdb
Pedro Mariano (fcul) <plmariano@fc.ul.pt>
2014-10-07 22:30:37 +0100

    Changed internal representation of weights to ints.

--- mercury/simulations.m
+++ mercury/simulations.m
@@ -15,9 +15,10 @@
 
 :- implementation.
 
-:- import_module ebea, ebea.streams, ebea.population, ebea.population.configuration, ebea.population.site, ebea.population.site.parameters.
+:- import_module ebea, ebea.streams, ebea.player, ebea.player.chromosome, ebea.player.selection, ebea.player.selection.chromosome, ebea.player.selection.pcv, ebea.population, ebea.population.configuration, ebea.population.site, ebea.population.site.parameters.
 :- import_module data, data.config, data.config.io, data.config.pretty, data.seed, data.prng.
 :- import_module gl, gl.pgp, gl.pgp.game.
+:- import_module gl, gl.'2x2', gl.'2x2'.game.
 :- import_module dir, list, maybe, solutions, string.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -57,7 +58,7 @@ main(!IO) :-
 	),
 	(	%
 		MConfig = ok(Config),
-		solutions.unsorted_aggregate(config(Config), simulations.run, !IO)
+		solutions.unsorted_aggregate(config(Config), simulations.write, !IO)
 	;	
 		MConfig = error(Msg),
 		io.print(Msg, !IO),
@@ -81,17 +82,70 @@ config(Config, Result) :-
 		GameConfig0,
 		'sites :='(
 			GameConfig0^initialPopulation,
-			list.map(changeRandomPartnerSelection, GameConfig0^initialPopulation^sites))),
-	Cfg0 = 'pgp :='(Config, GameConfig0),
+			list.map(changePartnerSelection_site(normalPartnerSelection(partnerSelection(4, 7, 0.5, 0.5))), GameConfig0^initialPopulation^sites))),
+%			list.map(changePartnerSelection_site(weightedPartnerSelection(partnerSelection(4, 7, 0.5, 0.5))), GameConfig0^initialPopulation^sites))),
+	Cfg0 = 'pgp :='(Config, GameConfig1),
+	Cfg1 = 'level :='(Cfg0, ebea.streams.summary),
+	Cfg2 = 'random :='(Cfg1, mt(clock)),
+	Cfg3 = 'numberIterations :='(Cfg2, 10000),
+	Directory = string.format("D%i-%f", [i(Game^players), f(Game^provisionCost)]),
+	Result = result(Directory, Cfg3)
+	;
+	Config^selectedGame = '2x2',
+	list.member(Temptation, [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8]),
+	list.member(Sucker, [-0.2, -0.4, -0.6, -0.8, 0.0, 0.2, 0.4, 0.6, 0.8]),
+	Game = gl.'2x2'.game.initGame(Temptation, Sucker),
+	GameConfig0 = 'game :='(Config^cfg_2x2, Game),
+	GameConfig1 = 'initialPopulation :='(
+		GameConfig0,
+		'sites :='(
+			GameConfig0^initialPopulation,
+			list.map(changePartnerSelection_site(weightedPartnerSelection(partnerSelection(4, 7, 0.5, 0.5))), GameConfig0^initialPopulation^sites))),
+	Cfg0 = 'cfg_2x2 :='(Config, GameConfig1),
 	Cfg1 = 'level :='(Cfg0, ebea.streams.summary),
 	Cfg2 = 'random :='(Cfg1, lcg(clock)),
-	Directory = string.format("D%i-%f", [i(Game^players), f(Game^provisionCost)]),
-	Result = result(Directory, Cfg2)
+	Cfg3 = 'numberIterations :='(Cfg2, 10000),
+	Directory = string.format("D%f_%f", [f(Game^temptation), f(Game^sucker)]),
+	Result = result(Directory, Cfg3)
 	.
 
-:- func changeRandomPartnerSelection(ebea.population.site.parameters.parameters(CS)) = ebea.population.site.parameters.parameters(CS).
+:- func changeNormalPartnerSelection_site(ebea.population.site.parameters.parameters(CS)) = ebea.population.site.parameters.parameters(CS).
 
-changeRandomPartnerSelection(V) = V.
+changeNormalPartnerSelection_site(V) = 'chromosomes :='(V, list.map(changeNormalPartnerSelection_chromosome, V^chromosomes)).
+
+:- func changeNormalPartnerSelection_chromosome(ebea.population.site.parameters.initialPlayers(CS)) = ebea.population.site.parameters.initialPlayers(CS).
+
+changeNormalPartnerSelection_chromosome(V) = Result :-
+	Chromosome = ebea.player.chromosome.'selectionGenes :='(
+		V^chromosome,
+		normalPartnerSelection(partnerSelection(4, 7, 0.5, 0.5))),
+%	Result = 'quantity :='(V, 10)
+	Result = 'chromosome :='(V, Chromosome)
+	.
+
+
+
+
+:- func changePartnerSelection_site(
+	ebea.player.selection.chromosome.chromosome,
+	ebea.population.site.parameters.parameters(CS)) = ebea.population.site.parameters.parameters(CS).
+
+changePartnerSelection_site(PS, V) = 'chromosomes :='(V, list.map(changePartnerSelection_chromosome(PS), V^chromosomes)).
+
+:- func changePartnerSelection_chromosome(
+	ebea.player.selection.chromosome.chromosome,
+	ebea.population.site.parameters.initialPlayers(CS)
+	) = ebea.population.site.parameters.initialPlayers(CS).
+
+changePartnerSelection_chromosome(PS, V) = Result :-
+	Chromosome = ebea.player.chromosome.'selectionGenes :='(
+		V^chromosome,
+		PS),
+	V0 = 'quantity :='(V, 10),
+	Result = 'chromosome :='(V0, Chromosome)
+	.
+
+
 
 :- pred run(simulations.result, io.state, io.state).
 :- mode run(in, di, uo) is det.
